6f581b0f8023c797e771af1be56da22ea723fd8f
Dika <al.ramdhan312@gmail.com>
2025-08-19 14:43:10 +0700

    update web

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/<name>')
 def hello(name):
-    return render_template('index.html', name=name)
+    return render_template('home/index.html', name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
