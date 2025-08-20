0821cb32039ce0dea1b9e1e0c4d1e702c4157a48
Dika <al.ramdhan312@gmail.com>
2025-08-20 14:45:12 +0700

    add theme and styling login

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["DEBUG"] = True
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return render_template('auth/login/login.html');
+
+@app.route('/register')
+def register():
+    return render_template('auth/register/register.html');
 
 @app.route('/<name>')
 def hello(name):
